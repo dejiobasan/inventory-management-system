c467dd54582dff50e854c218e866865f013b04f5
Ayodeji Obasan <dejiobasan02@gmail.com>
2025-10-14 13:23:46 +0100

    feat: starter files

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-import express from "express"
+import express from "express";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 8000;
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
